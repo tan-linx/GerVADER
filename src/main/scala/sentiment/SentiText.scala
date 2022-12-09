9f5279c3a57fdfe7787246a76ad5b88728c84a82
“tan-linx” <tanjasadt@gmail.com>
2022-12-09 13:48:34 +0100

    Add tests for getWordsAndEmoticons (Tokenization)

--- src/main/scala/sentiment/SentiText.scala
+++ src/main/scala/sentiment/SentiText.scala
@@ -4,39 +4,21 @@ import sentiment.utils.SentimentUtils
 import util.control.Breaks._
 
 private[sentiment] class SentiText(val text: String) {
-
   val wordsAndEmoticons: Seq[String] = getWordsAndEmoticons()
   val isCapDifferential: Boolean = SentimentUtils.allCapDifferential(wordsAndEmoticons)
 
   /**
-   * Returns mapping of the form
-   * {'cat,': 'cat'},
-   * {',cat': 'cat'}
-   *
-   * @return
-   */
-  def wordsPlusPunc(): Map[String, String] = {
-    val noPuncText: String = removePunctuation(text)
-    val wordsOnly = noPuncText.split(" ").filter(x => x.length > 1)
-
-    //for each word in wordsOnly, get each possible variant of punclist before/after
-    //Seems poor. It can be improved in future.
-    var puncDic: Map[String, String] = Map[String, String]()
-
-    breakable {
-      for (word <- wordsOnly) {
-        for (punc <- SentimentUtils.puncList) {
-          if (puncDic.contains(word + punc)) {
-            break()
-          }
-
-          puncDic += (word + punc -> word)
-          puncDic += (punc + word -> word)
-        }
-      }
-    }
-
-    puncDic
+    Removes all trailing and leading punctuation
+    If the resulting string has two or fewer characters,
+    then it was likely an emoticon, so return original string
+    (ie ":)" stripped would be "", so just return ":)"
+  */
+  private def stripPuncIfWord(token: String, puncList: List[String]): String= {
+    val punc = puncList.head
+    val stripped = token.stripPrefix(punc).stripSuffix(punc)
+    if (puncList.tail == List.empty) stripped
+    else if (stripped.length <= 2) return token
+    else stripPuncIfWord(stripped, puncList.tail)
   }
 
   /**
@@ -46,20 +28,6 @@ private[sentiment] class SentiText(val text: String) {
    */
   private def getWordsAndEmoticons(): List[String] = {
     var wes: List[String] = text.split(" ").filter(x => x.length > 1).toList
-    val wordsPuncDic = wordsPlusPunc()
-
-    for (i <- wes.indices) {
-      if (wordsPuncDic.contains(wes(i))) {
-        wes = wes.updated(i, wordsPuncDic(wes(i)))
-      }
-    }
-
-    wes
+    wes.map(x => stripPuncIfWord(x, SentimentUtils.puncList))
   }
-
-  /**
-   * Removes punctuation from word
-   */
-  private def removePunctuation(text: String): String = text.filter(c => !SentimentUtils.puncList.contains(c.toString))
-
 }
--- src/test/scala/SentiTextTest.scala
+++ src/test/scala/SentiTextTest.scala
@@ -10,13 +10,34 @@ import sentiment.SentiText
 
 class SentiTextTest extends AnyFlatSpec with should.Matchers {
 
-  "SentiText" should "wordsPlusPunc" in {
-    val sentiText: SentiText = new SentiText("VADER")
-    val result = sentiText.wordsPlusPunc()
-    print(result)
-    result shouldEqual HashMap("VADER??" -> "VADER", "VADER'" -> "VADER", ";VADER" -> "VADER", "VADER?!?" -> "VADER", "??VADER" -> "VADER", "!!!VADER" -> "VADER", "VADER." -> "VADER", "!VADER" -> "VADER", "VADER;" -> "VADER", "VADER?" -> "VADER", "!!VADER" -> "VADER", "VADER!" -> "VADER", "-VADER" -> "VADER", ",VADER" -> "VADER", "VADER!!" -> "VADER", "VADER," -> "VADER", "?!?VADER" -> "VADER", "VADER?!?!" -> "VADER", "?VADER" -> "VADER", "'VADER" -> "VADER", "VADER???" -> "VADER", "VADER" -> "VADER", ":VADER" -> "VADER", "!?!?VADER" -> "VADER", "VADER-" -> "VADER", "!?!VADER"-> "VADER", "VADER:" -> "VADER", "VADER!?!" -> "VADER", "VADER" -> "VADER", "?!?!VADER" -> "VADER", "VADER!!!" -> "VADER", "???VADER" -> "VADER", ".VADER" -> "VADER", "VADER!?!?" -> "VADER")
-  }
+  "SentiText" should "getWordsAndEmoticons (Tokenization)" in {
+    val puncAndEmojiText = new SentiText(".Removes :leading: and ,trailing !puncutation!!! :)")
+    puncAndEmojiText.wordsAndEmoticons shouldEqual Seq("Removes", "leading", "and", "trailing", "puncutation", ":)")
 
-  "SentiText" should "getWordsAndEmoticons" in {
+    val emojiAndPuncText = new SentiText("Removes leading and trailing puncutation :)!")
+    emojiAndPuncText.wordsAndEmoticons shouldEqual Seq("Removes", "leading", "and", "trailing", "puncutation", ":)!")
+
+    val emojiText = new SentiText("Leaves most emoticons >:-)")
+    emojiText.wordsAndEmoticons shouldEqual Seq("Leaves", "most", "emoticons", ">:-)")
+
+    val contractionText = new SentiText("VADER-Algorithms leaves contractions")
+    contractionText.wordsAndEmoticons shouldEqual Seq("VADER-Algorithms", "leaves", "contractions")
+
+    // "||-:"" does not get recognized
+    val pureEmojiText = new SentiText("{:  >:-) |-0 |-: |-o |= |^: |o: |;-)")
+    pureEmojiText.wordsAndEmoticons shouldEqual Seq("{:", ">:-)", "|-0","|-:", "|-o", "|=", "|^:", "|o:", "|;-)")
+
+    val puncPlusLetterEmoji = new SentiText("Does not preserve punc-plus-letter emoticons :D")
+    puncPlusLetterEmoji.wordsAndEmoticons shouldEqual Seq("Does", "not", "preserve", "punc-plus-letter", "emoticons", ":D")
+
+    val abbreviations = new SentiText("ca. 100.000 Jahre 1.Klasse 1. Klasse")
+    abbreviations.wordsAndEmoticons shouldEqual Seq("ca.", "100.000", "Jahre", "1.Klasse", "1.", "Klasse")
+
+    val sentiText = new SentiText("The book was kind of good.")
+    sentiText.wordsAndEmoticons shouldEqual Seq("The", "book", "was", "kind", "of", "good")
+
+    // this sentence fails on @zirayal project due to tokenization
+    val complexSentiText = new SentiText("The plot was good, but the characters are uncompelling and the dialog is not great.")
+    complexSentiText.wordsAndEmoticons shouldEqual Seq("The", "plot", "was", "good", "but", "the", "characters", "are", "uncompelling", "and", "the", "dialog", "is", "not", "great")
   }
 }
