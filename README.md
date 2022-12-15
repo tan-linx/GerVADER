b703dacafb6e77ce7521813fca7b531b13b78994
“tan-linx” <tanjasadt@gmail.com>
2022-12-15 17:58:52 +0100

    Fix README.txt

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Sentiment
-German Sentiment analysis with GerVADER with Scala
+English Sentiment analysis with VADER using Scala
 
 ### Credits
 > Hutto, C.J. & Gilbert, E.E. (2014). VADER: A Parsimonious Rule-based Model for Sentiment Analysis of Social Media Text. Eighth International Conference on Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.
-
-> Karsten Michael Tymann, Matthias Lutz, Patrick Palsbröker and Carsten Gips: GerVADER - A German adaptation of the VADER sentiment analysis tool for social media texts. In Proceedings of the Conference "Lernen, Wissen, Daten, Analysen" (LWDA 2019), Berlin, Germany, September 30 - October 2, 2019.
 
 > Ziya Sarikaya, Sentiment: Sentiment analysis using VADER in Scala, (2017), GitHub repository,
 https://github.com/ziyasal/Sentiment
--- src/test/scala/SentimentIntensityAnalyserTest.scala
+++ src/test/scala/SentimentIntensityAnalyserTest.scala
@@ -209,33 +209,4 @@ class SentimentIntensityAnalyserTest extends AnyFlatSpec with should.Matchers {
     emoji_lexikon("🤣") shouldEqual "rolling on the floor laughing"
     emoji_lexikon("😂") shouldEqual "face with tears of joy"
   }
-
-  /**
-    "A SentimentIntensityAnalyzer" should "calculate polarity scores (German)" in {
-    val analyzer = new SentimentIntensityAnalyzer
-
-    val testGood = analyzer.polarityScores("GerVADER hat viel Potential <3")
-    testGood.negative shouldEqual 0.0
-    testGood.neutral shouldEqual 0.58
-    testGood.positive shouldEqual 0.42
-    testGood.compound shouldEqual 0.4404
-
-    val testGood2 = analyzer.polarityScores("Fußball entfacht in mir ein Feuer der Leidenschaft.")
-    testGood2.negative shouldEqual 0.174
-    testGood2.neutral shouldEqual 0.496
-    testGood2.positive shouldEqual 0.331
-    testGood2.compound shouldEqual 0.4404
-
-    val testNegative = analyzer.polarityScores("gwroße zeitverschwendung heute >:/")
-    testNegative.negative shouldEqual 0.726
-    testNegative.neutral shouldEqual 0.274
-    testNegative.positive shouldEqual 0.0
-    testNegative.compound shouldEqual -0.6486
-
-    val testNegative2 = analyzer.polarityScores("ich finde das überhaupt nicht gut")
-    testNegative2.negative shouldEqual 0.338
-    testNegative2.neutral shouldEqual 0.662
-    testNegative2.positive shouldEqual 0.0
-    testNegative2.compound shouldEqual -0.3724
-  } */
 }
